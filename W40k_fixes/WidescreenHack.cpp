964906a29efac3fc7a91fd56f738656cf8412cb4
SuicideMachine <SuiMachine@users.noreply.github.com>
2019-09-05 12:11:15 +0200

    Hor+ FOV and ini read

--- W40k_fixes/WidescreenHack.cpp
+++ W40k_fixes/WidescreenHack.cpp
@@ -1,4 +1,7 @@
 #include "WidescreenHack.h"
+# define M_PI           3.14159265358979323846  /* pi */
+#include <math.h>
+
 
 WidescreenHack::WidescreenHack()
 {
@@ -14,5 +17,10 @@ void WidescreenHack::SetAspect(float aspect)
 void WidescreenHack::OverrideFOV(float fov)
 {
 	HMODULE mod = GetModuleHandle(NULL);
-	*(float*)((intptr_t)mod + 0x29F938) = fov * 0.01745329300562541;
+	double dHorizontalRadians = M_PI * fov / 180;
+	double dVerticalRadians = 2 * atan(tan(dHorizontalRadians / 2) * (3.0 / 4.0));
+	//printf("Initial FOV calculation (4:3): %.3d (hor) %.3d (vert)\n", dHorizontalRadians, dVerticalRadians);
+	dHorizontalRadians = 2 * atan(tan(dVerticalRadians / 2) * this->aspect);
+
+	*(float*)((intptr_t)mod + 0x29F938) = dHorizontalRadians;
 }
